ba3088876b85eba5380d09c8acd002b413cc3151
jk-coder-academy <james.knight@coderacademy.edu.au>
2021-09-21 12:17:06 +1000

    DRYYYY

--- Encounter.rb
+++ Encounter.rb
@@ -6,8 +6,30 @@ class Encounter
     attr_reader :consequence # The consequence of the players choice of options.
     attr_reader :encounter_intro # Intro for the encounter!
 
+    def print_intro(player_name)
+        puts "#{player_name} encounters a #{@name}."
+        puts @art
+        puts @encounter_intro
+    end
+
+    def print_options()
+        options_index = 0
+        for option in @options
+            puts "#{options_index + 1}. #{option}"
+            options_index += 1
+        end
+    end
+
+    def get_player_choice()
+        print "Enter your choice: "
+        players_choice = gets.to_i - 1 # 0->3
+        puts @consequences[players_choice]
+
+        return players_choice
+    end
+
     # All encounters have a perform method.
     def perform(player)
-        # empty.
+        raise "Every encounter child should have their own 'perform' method."
     end
 end
